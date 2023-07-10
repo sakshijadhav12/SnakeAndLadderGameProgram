629fa80d1d7759c9fe4ae419d80f4785af659f68
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-10 08:20:14 +0530

    [sakshi] modifying program.cs to add feature of roll a dice

--- SnakeAndLadderGameProgram/Program.cs
+++ SnakeAndLadderGameProgram/Program.cs
@@ -1,12 +1,44 @@
 ﻿namespace SnakeAndLadderGameProgram
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            int position = 0;// variable
-            Console.WriteLine("Hello,!Welcome to Snake and Ladder Game");
-            Console.WriteLine("Single player at start, position: " + position);
+            Game game = new Game();
+            game.Start();
         }
     }
+
+    class Game
+    {
+        int position;
+
+        public void Start()
+        {
+            position = 0;
+
+            Console.WriteLine("Hello!Welcome to Snake and Ladder Game");
+            Console.WriteLine("Single player at start, position:" + position);
+
+            Console.WriteLine("Press  Enterkey to roll the dice...");
+            Console.ReadKey();
+
+            int diceRoll = Dice.Roll();
+            Console.WriteLine("Dice rolled: " + diceRoll);
+
+            position += diceRoll;
+
+            Console.WriteLine("Player moves to position: " + position);
+        }
+        class Dice
+        {
+            public static int Roll()
+            {
+                Random random = new Random();
+                return random.Next(1, 7); // Returns a random number between 1 and 6
+            }
+        }
+    }
+
 }
