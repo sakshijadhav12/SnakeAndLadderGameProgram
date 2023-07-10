7c52257c5ef75f39af7d74c7f3b77589562762c6
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-10 08:09:06 +0530

    [sakshi] creating a snake and ladder game

---
+++ SnakeAndLadderGameProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadderGameProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello !Welcome to the Sanke and Ladder Game");
+        }
+    }
+}
